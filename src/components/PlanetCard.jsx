64b4685d923f1cdc75913baa956a00f6676f3e24
Pedro Viana <pedroviana.developer13@gmail.com>
2022-11-25 16:04:49 -0300

    lint requisito 5

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -7,7 +7,7 @@ class PlanetCard extends React.Component {
     return (
       <div data-testid="planet-card">
         <p data-testid="planet-name">{planetName}</p>
-        <img alt={ `Planeta ${planetName}` } src={ planetImage }/>
+        <img alt={ `Planeta ${planetName}` } src={ planetImage } />
       </div>
     );
   }
